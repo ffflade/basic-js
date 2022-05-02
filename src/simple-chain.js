93472dc04855f89418499ad8953fd9a68429eded
Rebenchuk Timur <timurlandiya@mail.ru>
2022-05-02 18:51:55 +0300

    feat: solve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,26 +5,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	chain: [],
+
+	addLink(n) {
+		if (n == null || n == undefined) {
+			this.chain.push(`()`);
+			return (this);
+		} else {
+			this.chain.push(`(${n})`);
+			return (this);
+		}
+	},
+	getLength() {
+		let len =  0;
+		for (let i = 0; i < this.chain.length; i++) {
+			if (Number.isInteger(+(this.chain[i])) && this.chain[i] !== ' ') {
+				len += 1;
+			}
+		};
+
+		return (len)
+	},
+	removeLink(num) {
+		if (num !== Math.trunc(num) || num <= 0 || num > this.chain.length || !Number.isInteger(num)) {
+			throw new Error('ERROR: NEEDED NUMBER')
+		}
+		this.chain.splice(num - 1, 1);
+		return (this);
+	},
+	reverseChain() {
+		this.chain = this.chain.reverse();
+		return (this);
+	},
+	finishChain() {
+		let res = this.chain;
+		this.chain.length = 0;
+
+		return (res.join(' ~~ '));
+	},
 };
 
 module.exports = {
