36be60a1c81eef6edd7c70342a44fdc597fc3936
Rebenchuk Timur <timurlandiya@mail.ru>
2022-05-02 13:30:27 +0300

    feat: solve hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -18,7 +18,7 @@ function calculateHanoi(disksNumber, turnsSpeed) {
 	return {
 		turns: Math.pow(2, disksNumber) - 1,
 		seconds: Math.floor(3600 * (Math.pow(2, disksNumber) - 1) / turnsSpeed),
-	}
+	};
 }
 
 module.exports = {
