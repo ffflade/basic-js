ae80841b8579329b9a364838e4bdbee9231de763
Rebenchuk Timur <timurlandiya@mail.ru>
2022-05-02 13:12:10 +0300

    feat: solve dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+	if (!Array.isArray(members)) return false;
+
+	return members.reduce((acc, curr) => {
+		return curr && typeof curr === 'string' ? acc + curr.trim()[0] : acc;
+	},'')
+		.toUpperCase()
+		.split('')
+		.sort()
+		.join('');
 }
 
 module.exports = {
